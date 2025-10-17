16945ad48125a9b3812e5d93e74e4649c8d901a7
Gousiya5289 <gousiya5289@gmail.com>
2025-10-17 23:11:32 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,25 +1,16 @@
-output "aks_id" {
-  value = azurerm_kubernetes_cluster.aks.id
-}
-
-output "aks_fqdn" {
-  value = azurerm_kubernetes_cluster.aks.fqdn
-}
-
-output "aks_node_rg" {
-  value = azurerm_kubernetes_cluster.aks.node_resource_group
-}
-
-output "acr_id" {
-  value = azurerm_container_registry.acr.id
+output "resource_group_name" {
+  value = azurerm_resource_group.aks_rg.name
 }
 
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
 
-resource "local_file" "kubeconfig" {
-  depends_on   = [azurerm_kubernetes_cluster.aks]
-  filename     = "kubeconfig"
-  content      = azurerm_kubernetes_cluster.aks.kube_config_raw
+output "aks_fqdn" {
+  value = azurerm_kubernetes_cluster.aks.fqdn
+}
+
+output "aks_kubeconfig" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
 }
